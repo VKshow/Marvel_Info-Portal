afeefe0ed11ade5ee087f480c12920d04a153c0c
vk <justvk2013@gmail.com>
2023-11-20 20:07:17 -0700

    load more added

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from "react";
+
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -124,5 +125,7 @@ const View = ({ char }) => {
     </>
   );
 };
+
+
 
 export default CharInfo;
